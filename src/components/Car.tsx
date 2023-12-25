082a4e6d10e0fb1d07df9cac00529c8257230594
Mudit Jain <muditert34@gmail.com>
2023-12-25 13:36:04 +0530

    responsive - 1

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -28,23 +28,23 @@ const Car = () => {
 
   return (
     <>
-      <h3 className="text-8xl font-bold w-3/4 mx-auto text-center mt-32 mb-32">
+      <h3 className="text-3xl sm:text-5xl md:text-7xl lg:text-8xl font-bold w-3/4 mx-auto text-center mt-32 mb-32">
         IDEAL LOCATION FOR LIVING IN HARMONY WITH NATURE
       </h3>
-      <div className="flex flex-row justify-around mx-auto  items-center w-3/5 h-full mt-10">
+      <div className="flex flex-row  mx-auto justify-between lg:justify-around w-full -blue-500 md:w-4/5 lg:w-3/5 h-[85%] sm:h-full mt-10">
         {/* first div */}
 
-        <div className="flex flex-col gap-52 mt-52">
+        <div className="flex flex-col w-[15%] md:w-[15%] lg:w-[20%] gap-52 mt-52 -slate-500">
           {/* loop this */}
           <div className="flex flex-col justify-center items-center gap-4">
             <Image
               src={dest1}
               alt="nil"
-              className="rounded-full border-spacing-3 border-orange-600 border-2 p-1"
+              className="rounded-full border-spacing-3 border-orange-600 border-2 p-[0.1rem] sm:p-1"
               height={100}
               width={100}
             />
-            <p className="font-regular w-3/5 text-center text-xs">
+            <p className="font-regular w-4/5 md:w-3/5 text-center text-[0.3rem] md:text-xs">
               The Newest Town and local storer
             </p>
           </div>
@@ -52,11 +52,11 @@ const Car = () => {
             <Image
               src={dest2}
               alt="nil"
-              className="rounded-full border-spacing-3 border-orange-600 border-2 p-1"
+              className="rounded-full border-spacing-3 border-orange-600 border-2 p-[0.1rem] sm:p-1"
               height={100}
               width={100}
             />
-            <p className="font-regular w-3/5 text-center text-xs">
+            <p className="font-regular w-4/5 md:w-3/5 text-center text-[0.3rem] md:text-xs">
               Sports facilities and classes
             </p>
           </div>
@@ -64,18 +64,18 @@ const Car = () => {
             <Image
               src={dest3}
               alt="nil"
-              className="rounded-full border-spacing-3 border-orange-600 border-2 p-1"
+              className="rounded-full border-spacing-3 border-orange-600 border-2 p-[0.1rem] sm:p-1"
               height={100}
               width={100}
             />
-            <p className="font-regular w-3/5 text-center text-xs">
+            <p className="font-regular w-4/5 md:w-3/5 text-center text-[0.3rem] md:text-xs">
               Lakeview Conserv. Area
             </p>
           </div>
         </div>
 
         {/* second div */}
-        <div className="relative flex justify-center gap-3 mr-[19rem]">
+        <div className="-red-600 relative lg:w-2/5 sm:w-1/2 w-1/2">
           <motion.div
             initial={{ y: 0 }}
             whileInView={{
@@ -88,26 +88,30 @@ const Car = () => {
             }}
           >
             <Image
-              className="relative top-[-3.4rem] left-[9rem] h-[29rem]"
+              className="absolute top-[-3.4rem] h-[15rem] sm:h-[29rem] w-[335px]"
               src={car}
               alt={"nil"}
-              width={350}
             />
           </motion.div>
-          <div className="flex flex-col gap-[20rem] bg-slate-500 w-[0.004rem] justify-center items-center">
-            <div className="w-4 h-4 rounded-full bg-orange-500 border-red-600 border-[1.5px]"></div>
-            <div className="w-4 h-4 rounded-full bg-orange-500 border-red-600 border-[1.5px]"></div>
-            <div className="w-4 h-4 rounded-full bg-orange-500 border-red-600 border-[1.5px]"></div>
-            <div className="w-4 h-4 rounded-full bg-orange-500 border-red-600 border-[1.5px]"></div>
+          <div className="flex mx-auto flex-col gap-[20rem] -slate-500 w-[0.004rem] justify-center items-center">
+            <div className="w-4 h-4 rounded-full -orange-500 border-red-600 border-[1.5px]"></div>
+            <div className="w-4 h-4 rounded-full -orange-500 border-red-600 border-[1.5px]"></div>
+            <div className="w-4 h-4 rounded-full -orange-500 border-red-600 border-[1.5px]"></div>
+            <div className="w-4 h-4 rounded-full -orange-500 border-red-600 border-[1.5px]"></div>
           </div>
         </div>
 
         {/* third div */}
-        <div className="flex flex-col gap-[18rem] mt-40">
+        <div className="flex flex-col gap-[18rem] w-[20%] md:w-[20%] lg:w-[20%] mt-52 -yellow-400">
           {TimeFromCar.map((item, index) => (
-            <div key={index} className="flex flex-row gap-4 items-center">
-              <h3 className="text-7xl">{item.time}</h3>
-              <p className="w-1/2 text-sm">{item.title}</p>
+            <div
+              key={index}
+              className="flex flex-row gap-1 lg:gap-4 items-center"
+            >
+              <h3 className="text-xl sm:text-4xl lg:text-7xl">{item.time}</h3>
+              <p className="w-3/4 lg:w-1/2 text-[0.3rem] md:text-xs lg:text-sm">
+                {item.title}
+              </p>
             </div>
           ))}
         </div>
